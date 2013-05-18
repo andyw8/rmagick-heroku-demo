4f0b743345fcbecdc673070adc9280987817cac5
Andy Waite <github.aw@andywaite.com>
2013-05-18 19:19:30 +0100

    Serving an image

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source "https://rubygems.org"
 
 gem "rmagick"
 gem "sinatra"
+
+group :development do
+  gem "shotgun"
+end
--- home.rb
+++ home.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
+require 'RMagick'
 
 get '/' do
-  "Hello World!"
+  content_type 'image/png'
+  canvas = Magick::Image.new(240, 300,
+                Magick::HatchFill.new('white','lightcyan2'))
+  gc = Magick::Draw.new
+
+  gc.stroke('#001aff')
+  gc.stroke_width(3)
+  gc.fill('#00ff00')
+
+  x = 120
+  y = 32.5
+  gc.polygon(x,    y,     x+29, y+86,  x+109, y+86,
+             x+47, y+140, x+73, y+226, x,     y+175,
+             x-73, y+226, x-47, y+140, x-109, y+86,
+             x-29, y+86)
+
+  gc.draw(canvas)
+  canvas.format = 'png'
+  canvas.to_blob
 end
